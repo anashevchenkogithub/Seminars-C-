3a0c9e805c6c6b6b8ee64f8c11b8127ec31767a8
Ana Shevchenko <shevchenko_a@list.ru>
2023-01-14 22:19:50 +0300

    Homework 1 14.01.23

---
+++ Seminar 1/Task 1/Program.cs
@@ -0,0 +1,29 @@
+﻿/* Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3 */
+
+Console.Write("Введите целое число  > ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите еще одно целое число  > ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+
+if (a < b) 
+{ Console.Write($"Наибольшее число  {b}");}
+
+else
+{
+    if (a == b) {
+        Console.Write($"Оба числа равны  {b}");} 
+    else {
+        Console.Write($"Наибольшее число  {a}");
+    }
+
+    
+}
+
+
+
---
+++ Seminar 1/Task 2/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22 */
+
+Console.Write("Введите целое число  > ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите еще одно целое число  > ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье целое число  > ");
+int с = Convert.ToInt32(Console.ReadLine());
+
+int[] numbers = new int[] {a, b, с};
+int i = 0;
+int max = a;
+
+while (i < 3) 
+{ 
+    if (numbers[i]> max) {
+        max = numbers[i];
+        i++; }
+    else {i++;}
+}
+
+Console.WriteLine($"Наибольшее из заданных число {max}");
