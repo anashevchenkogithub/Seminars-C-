9564ee1f0c8b4105faf2a1b06ab79a62e3236691
Ana Shevchenko <shevchenko_a@list.ru>
2023-01-21 22:19:20 +0300

    Homework2

---
+++ Seminar 2/Task3/Program.cs
@@ -0,0 +1,31 @@
+﻿//Напишите программу, которая принимает на вход цифру, 
+//обозначающую день недели, и проверяет, является ли этот день выходным.
+
+//6 -> да
+//7 -> да
+//1 -> нет
+
+int InputInt(string message)
+{
+
+    Console.Write(message);
+    int x = Convert.ToInt32(Console.ReadLine());
+    return x;
+}
+int DayNumber = InputInt("Введите число от 1 до 7, обозначающее день недели > ");
+
+if (!(DayNumber > 7 || DayNumber < 1))
+{
+    if (DayNumber == 6 || DayNumber == 7)
+    {
+        Console.WriteLine($"Это выходной день");
+    }
+    else
+    {
+        Console.WriteLine($"Это будний день");
+    }
+}
+else
+{
+    Console.WriteLine($"Число {DayNumber} не подходит");
+}
