85ec79c3e9779fdbd99d1d707aa23ba4e3440161
Ana Shevchenko <shevchenko_a@list.ru>
2023-01-21 23:38:58 +0300

    Homework3

---
+++ Seminar 3/Task3/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Напишите программу, которая принимает на вход число (N)
+ и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+int Prompt(string message)
+{
+    Console.Write($"{message}: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+int number = Prompt("Введите число ");
+
+
+int i = 1;
+
+while (i <= number)
+{
+    double cube = Math.Pow(i, 3);
+    Console.WriteLine($"Число {i} в кубе равно {cube}");
+    i++;
+}
