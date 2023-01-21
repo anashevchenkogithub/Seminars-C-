4c418e2f38cbb1f241d5900103a81ceb007866d7
Ana Shevchenko <shevchenko_a@list.ru>
2023-01-21 22:32:51 +0300

    Homework2

---
+++ Seminar 2/Task4/Program.cs
@@ -0,0 +1,10 @@
+﻿/* Напишите программу, которая выводит случайное трёхзначное число
+ и удаляет вторую цифру этого числа. Не использовать строки для расчета
+ */
+
+
+int number = new Random().Next(100, 1000);
+int digit3 = number % 10;
+int digit1 = number / 100;
+int result = digit1 * 10 + digit3;
+Console.WriteLine($"Сгенерировано число {number} > без второй цифры {result}");
