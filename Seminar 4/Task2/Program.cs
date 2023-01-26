9505c5431ef299a0a9a40730c3d64644092ffc98
Ana Shevchenko <shevchenko_a@list.ru>
2023-01-26 13:48:04 +0300

    Homework4

---
+++ Seminar 4/Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе. 
+//Реализовать через функции.
+
+int InputInt(string msg)
+{
+    Console.WriteLine($"{msg} ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int DigitsSum(int numberf)
+{
+    int n = numberf;
+    int SumD = 0;
+    while (n > 0)
+    {
+        int ost = n % 10;
+        SumD = SumD + ost;
+        n = n / 10;
+    }
+    return SumD;
+}
+
+int number = InputInt($"Введите целое положительное число");
+int sum = DigitsSum(number);
+
+Console.WriteLine($"В числе {number} сумма цифр равна {sum}");
