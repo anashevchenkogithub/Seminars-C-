5bff32b9f976da609262e413db9a382ca0e3aa4b
Ana Shevchenko <shevchenko_a@list.ru>
2023-01-26 13:30:11 +0300

    Homework4

---
+++ Seminar 4/Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿// //Напишите цикл, который принимает на вход два числа (A и B) и 
+// возводит число A в натуральную степень B. Реализовать через функции.
+
+int InputInt(string msg)
+{
+    Console.Write($"{msg}: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int DegreeF(int numberf, int degreef)
+{
+    int result = 1;
+    for (int i = 0; i < degreef; i++)
+    {
+        result = numberf * result;
+    }
+    return result;
+}
+
+int number = InputInt("Введите число ");
+int degree = InputInt("Введите степень ");
+
+int result = DegreeF(number, degree);
+
+Console.WriteLine($"Число {number} в степени {degree} равно {result}");
