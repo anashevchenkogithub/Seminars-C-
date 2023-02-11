f2b15bc607780112cee599929cb0f6f722ef7f20
Ana Shevchenko <shevchenko_a@list.ru>
2023-02-11 14:50:10 +0300

    Homework8

---
+++ Seminar 8/Task2/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 2: Задайте прямоугольный двумерный массив. 
+//Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+void FillArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++) arr[i, j] = new Random().Next(1, 11);
+    }
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++) Console.Write($"{arr[i, j]}\t");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+(int min, int minIndex) MinRowSum(int[,] arr)
+{
+    int[] sumRows = new int[arr.GetLength(0)];
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++) sumRows[i] = sumRows[i] + arr[i, j];
+    }
+    int min = sumRows[0];
+    int minIndex = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        if (sumRows[i] < min) { min = sumRows[i]; minIndex = i; }
+    }
+    return (min, minIndex);
+}
+
+int[,] array = new int[3, 4];
+FillArray(array);
+PrintArray(array);
+
+(int min, int minIndex) = MinRowSum(array);
+Console.WriteLine($"Минимальная сумма элементов равна {min} и находится в строке {minIndex + 1}");
