b6e8708fa60426012ed8031ec54a4ee054f17686
Ana Shevchenko <shevchenko_a@list.ru>
2023-02-17 17:41:06 +0300

    Homework9

---
+++ Seminar 9/Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Задайте значения M и N. Напишите программу, которая выведет все чётные 
+натуральные числа в промежутке от M до N с помощью рекурсии.
+M = 1; N = 5 -> "2, 4"
+M = 4; N = 8 -> "4, 6, 8"
+*/
+void ShowNumbers(int start, int finish)
+{
+    if (start > finish) return;
+    if (start % 2 == 1) start = start + 1;
+    Console.Write(start + " ");
+    
+    ShowNumbers(start + 2, finish);
+}
+
+ShowNumbers(3,15);
